396892dcf4a7bc63b50dac5bb457151ea99a77c2
WillAnd31 <willimaman@gmail.com>
2015-09-26 13:28:22 -0600

    Changed port to port 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
-var port = 8000;
+var port = 80;
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/www'));
