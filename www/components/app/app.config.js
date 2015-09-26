184f0048c3cdda2fe568d6f9f7779f759959f446
WillAnd31 <willimaman@gmail.com>
2015-09-26 13:45:43 -0600

    Fixed module injectors

--- www/components/app/app.config.js
+++ www/components/app/app.config.js
@@ -7,11 +7,12 @@ angular
 configure.$inject = [
 	'$compileProvider',
 	'$stateProvider', 
+	'$locationProvider',
 	'$urlRouterProvider',
 	'$httpProvider'
 ];
 
-function configure($compileProvider, $stateProvider, $urlRouterProvider, $translateProvider, $httpProvider){
+function configure($compileProvider, $stateProvider, $locationProvider, $urlRouterProvider, $translateProvider, $httpProvider){
 
 	$urlRouterProvider.when('', 'home');
 	$locationProvider.html5Mode(true);
