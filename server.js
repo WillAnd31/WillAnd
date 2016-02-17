8edf99b042490006c57a8778905cf69549eb568a
WillAnd31 <will@willand.co>
2016-02-16 17:54:56 -0700

    testing

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var morgan = require('morgan');
-var port = 8000;
+var port = 80;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/www'));
 app.listen(port);
 
 app.get('*', function(req, res, next) {
-	res.sendFile('./www/index.html', { root: __dirname });
+    res.sendFile('./www/index.html', { root: __dirname });
+});
+
+app.post('/test', function (req, res, next) {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.get('/v1/oauth/authorize', function (req, res, next) {
+    console.log('*********************GET: ', req.body);
+    res.send(req.body);
+});
+
+app.get('/v1/oauth/token', function (req, res, next) {
+    console.log('*********************GET: ', req.body);
+    res.send(req.body);
+});
+
+app.get('/v1/oauth/me', function (req, res, next) {
+    console.log('*********************GET: ', req.body);
+    res.send(req.body);
+});
+
+app.post('/v1/oauth/authorize', function (req, res, next) {
+    console.log('*********************POST: ', req.body);
+    res.send(req.body);
+});
+
+app.post('/v1/oauth/token', function (req, res, next) {
+    console.log('*********************POST: ', req.body);
+    res.send(req.body);
+});
+
+app.post('/v1/oauth/me', function (req, res, next) {
+    console.log('*********************POST: ', req.body);
+    res.send(req.body);
 });
 
 console.log('Running on port: ' + port);
